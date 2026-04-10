63cf67f8b45d8f47693b52ab874c3766090c2f83
Guifré González Ceresuela <7696465005@edu.gencat.cat>
2026-04-10 16:00:20 +0200

    try

--- timeline.js
+++ timeline.js
@@ -1,20 +1,28 @@
 const timelineItems = document.querySelectorAll('.timeline-item');
 
-const observer = new IntersectionObserver((entries) => {
+const enterObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
-        } else {
-            entry.target.classList.remove('visible');
         }
-
     });
 }, { 
     threshold: 0,
     rootMargin: '-20% 0px -20% 0px'
 });
 
+const exitObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            entry.target.classList.remove('visible');
+        }
+    });
+}, { 
+    threshold: 0,
+    rootMargin: '-17% 0px -17% 0px'
+});
+
 timelineItems.forEach(item => {
-    observer.observe(item);
+    enterObserver.observe(item);
+    exitObserver.observe(item);
 });
